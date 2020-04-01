c7154553a84a042521345c0103547d91f58c2c40
BENZABOSS <joyceben33@gmail.com>
2020-03-31 21:03:10 -0400

    added socket example to help implement simulation.

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -3,23 +3,21 @@ import ReactDOM from 'react-dom';
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
-import rootReducer from "./reducers/index";
+// import rootReducer from "./reducers/index";
 import './index.css';
 import Home from './components/home';
 
 
 //Create store for state management
-const store = createStore(rootReducer, {}, applyMiddleware(thunk));
+// const store = createStore(rootReducer, {}, applyMiddleware(thunk));
 
+{/* <Provider store={store}>
+  <Home />
+</Provider> */}
 
 ReactDOM.render(
-<Provider store={store}>
-  <Home />
-</Provider>,
+<Home/>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
