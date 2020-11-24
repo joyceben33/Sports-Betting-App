7d4d9c4bfaf31bf3b63fff04b38b5416b247e4cf
BENZABOSS <joyceben33@gmail.com>
2020-11-24 10:21:55 -0500

    adjusted the way express serves up the react app.

--- server.js
+++ server.js
@@ -52,13 +52,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 if (process.env.NODE_ENV === 'production') {
     // Express will serve up production assets
     // like our main.js file, or main.css file!
-    app.use(express.static('client/build/'));
+    app.use(express.static(path.join(__dirname, "..", "build")))
+    app.use(express.static("public"));
 
     // Express will serve up the index.html file
     // if it doesn't recognize the route
     
-    app.get('/', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    app.use((req, res, next) => {
+        res.sendFile(path.join(__dirname, "..", "build", "index.html"));
     });
 }
 
