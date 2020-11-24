fd7191583f56eb1959278c2c9afb9345bda860bc
BENZABOSS <joyceben33@gmail.com>
2020-11-24 10:37:27 -0500

    added cors to the server.js file

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const mongoose = require('mongoose');
 const path = require('path');
 const bodyParser = require('body-parser');
-// const cors = require('cors');
+const cors = require('cors');
 // const router = require('./router');
 const app = express();
 
@@ -41,25 +41,25 @@ db.once('open', () => {
 
 
 //Data parsing
+app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 
 /***************************************************************************************** */
 /* Conditions for production														   */
 /***************************************************************************************** */
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
     // Express will serve up production assets
     // like our main.js file, or main.css file!
-    app.use(express.static(path.join(__dirname, "..", "build")))
     app.use(express.static("public"));
 
     // Express will serve up the index.html file
     // if it doesn't recognize the route
     
-    app.use((req, res, next) => {
-        res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "client", "build", "index.html"));
     });
 }
 
